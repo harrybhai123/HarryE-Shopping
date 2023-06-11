a42409cfd77bd1e06f4049dc32cbc5d5e015c08d
Hariom Tomar <68967590+harrybhai123@users.noreply.github.com>
2023-06-11 19:23:26 +0530

    added changes for deploy

--- server.js
+++ server.js
@@ -7,12 +7,19 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from "path"
+import { fileURLToPath } from "url";
 
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
+
+// es module fix
+const __filename = fileURLToPath(import.meta.url)
+
+const __dirname = path.dirname(__filename)
 
 //rest object
 const app = express();
@@ -21,6 +28,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, './client/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
@@ -28,8 +36,8 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
+app.get("*", function(req, res)  {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //PORT
